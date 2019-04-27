d09e0d0b4524fa82368514e9324c9cba3d301f20
Ferrydwiz <48443765+Ferrydwiz@users.noreply.github.com>
2019-04-27 04:56:08 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESPONSI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            karyawan krywn = new karyawan();
+
+            krywn.nama = "FERRY DWI Z";
+            krywn.nik = "2362";
+            krywn.gaji = 1000000;
+
+            krywn.nama2 = "JAMILAH";
+            krywn.nik2 = "1111";
+            krywn.gaji2 = 2000000;
+
+            krywn.infoGaji();
+            Console.WriteLine();
+            krywn.kenaikanGaji();
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
